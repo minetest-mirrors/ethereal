724ec65c13a476cc5987f22292b2887bc87bc8c6
tenplus1 <kinsellaja@yahoo.com>
2024-08-25 14:03:16 +0100

    16px mushroom sapling, new lemonade texture

--- food.lua
+++ food.lua
@@ -385,13 +385,14 @@ minetest.register_craft({
 minetest.register_node("ethereal:lemonade", {
 	description = S("Lemonade"),
 	drawtype = "plantlike",
+	visual_scale = 0.5,
 	tiles = {"ethereal_lemonade.png"},
 	inventory_image = "ethereal_lemonade.png",
 	wield_image = "ethereal_lemonade.png",
 	paramtype = "light",
 	walkable = false,
 	selection_box = {
-		type = "fixed", fixed = {-0.25, -0.5, -0.25, 0.25, 0.25, 0.25}
+		type = "fixed", fixed = {-0.15, -0.5, -0.15, 0.15, 0, 0.15}
 	},
 	groups = {vessel = 1, dig_immediate = 3, attached_node = 1, drink = 1},
 	on_use = minetest.item_eat(5, "vessels:drinking_glass"),
