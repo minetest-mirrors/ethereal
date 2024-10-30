3f35842f9854389cd60b2f022f4ea966d330a49e
tenplus1 <kinsellaja@yahoo.com>
2024-10-30 09:58:25 +0000

    alias bush3 to pine_needles_bush

--- leaves.lua
+++ leaves.lua
@@ -504,31 +504,17 @@ minetest.register_craft({
 	burntime = 1
 })
 
--- bush block #3
+-- pine needles bush (replaces bush 3)
 
-minetest.register_node("ethereal:bush3", {
-	drawtype = "allfaces_optional",
-	description = S("Bush #3"),
-	tiles = {"default_pine_needles.png"},
-	paramtype = "light",
-	walkable = true,
-	groups = {snappy = 3, flammable = 2},
-	sounds = default.node_sound_leaves_defaults()
-})
+minetest.register_alias("ethereal:bush3", "default:pine_bush_needles")
 
 minetest.register_craft({
-	output = "ethereal:bush3",
+	output = "default:pine_bush_needles",
 	recipe = {
 		{"group:leaves", "group:leaves", "group:leaves"},
 		{"group:leaves", "default:pine_needles", "group:leaves"},
 		{"group:leaves", "group:leaves", "group:leaves"}
 	}
-})
-
-minetest.register_craft({
-	type = "fuel",
-	recipe = "ethereal:bush3",
-	burntime = 1
 })
 
 -- basandra bush stem, leaves
