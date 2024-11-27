e8b378f1f51b99c436f146159ff66184597812a2
tenplus1 <kinsellaja@yahoo.com>
2024-11-27 08:03:25 +0000

    fix mushroom biome ambience

--- ambience.lua
+++ ambience.lua
@@ -11,13 +11,14 @@ ambience.add_set("ethereal_mushroom", {
 		{name = "ethereal_mushroom", length = 1.3, pitch = 1.2, ephemeral = true},
 	},
 
-	nodes = ({"ethereal:mushroom_trunk"}),
+	nodes = ({"ethereal:mushroom_trunk", "ethereal:mushroom_dirt"}),
 
 	sound_check = function(def)
 
 		local c = (def.totals["ethereal:mushroom_trunk"] or 0)
+		local d = (def.totals["ethereal:mushroom_dirt"] or 0)
 
-		if c > 30 then return "ethereal_mushroom" end
+		if c > 30 and d > 70 then return "ethereal_mushroom" end
 	end
 })
 
