4df190cf5d72bd6c09fb50433fc90ca42e6b981d
tenplus1 <kinsellaja@yahoo.com>
2025-05-25 17:24:43 +0100

    fix biome typos

--- decor.lua
+++ decor.lua
@@ -492,8 +492,8 @@ register_decoration(1, {
 	sidelen = 16, y_min = -10, y_max = -5,
 	noise_params = {offset = -0.04, scale = 0.1, spread = {x = 200, y = 200, z = 200},
 			seed = 87112, octaves = 3, persist = 0.7},
-	biomes = {"frost_ocean", "deciduous_forest_ocean", "sandstone_ocean", "swamp_ocean",
-			"snowy_grassland_ocean"},
+	biomes = {"frost_ocean", "deciduous_forest_ocean", "sandstone_desert_ocean",
+			"swamp_ocean", "snowy_grassland_ocean"},
 	flags = "force_placement",
 	decoration = "default:sand_with_kelp", place_offset_y = -1,
 	param2 = 48, param2_max = 96})
--- schems.lua
+++ schems.lua
@@ -285,7 +285,7 @@ register_decoration(ethereal.savanna, {
 register_decoration(1, {
 	place_on = "default:sand",
 	fill_ratio = 0.0025, y_min = 1, y_max = 1,
-	biomes = {"desert_ocean", "plains_ocean", "sandstone_ocean",
+	biomes = {"desert_ocean", "plains_ocean", "sandstone_desert_ocean",
 			"mesa_ocean", "grove_ocean", "deciduous_forest_ocean"},
 	schematic = ethereal.palmtree})
 
