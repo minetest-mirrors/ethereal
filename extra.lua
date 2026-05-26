02d51bcca9d00d59ec9077f1b7d345266ec8e1ee
tenplus1 <kinsellaja@yahoo.com>
2026-05-26 13:19:34 +0100

    add charcoal block ignite same as coal block

--- extra.lua
+++ extra.lua
@@ -440,7 +440,24 @@ core.register_node("ethereal:charcoal_block", {
 	tiles = {"ethereal_charcoal_block.png"},
 	is_ground_content = false,
 	groups = {cracky = 3},
-	sounds = default.node_sound_stone_defaults()
+	sounds = default.node_sound_stone_defaults(),
+	after_destruct = function(pos, oldnode)
+
+		pos.y = pos.y + 1
+
+		if core.get_node(pos).name == "fire:permanent_flame" then
+			core.remove_node(pos)
+		end
+	end,
+
+	on_ignite = function(pos, igniter)
+
+		local flame_pos = {x = pos.x, y = pos.y + 1, z = pos.z}
+
+		if core.get_node(flame_pos).name == "air" then
+			core.set_node(flame_pos, {name = "fire:permanent_flame"})
+		end
+	end
 })
 
 core.register_craft({
